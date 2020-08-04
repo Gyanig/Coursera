b37558917231427109a3313c4cf696442355b514
Parzival <gyanig.sahu@gmail.com>
2020-08-04 23:32:25 +0530

    Algorithms 1 work update

---
+++ Algorithms-1/C++/union.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class UF{
+	public:
+		int UF(int n);				 	// number of objects 0->(n-1)
+		void union(int p,int q);		// connect 
+		bool connected(int p,int q); 	// recursion till find the end or the q from p
+};
+
+
+int main(){
+	
+	
+	
+	return 0;
+}
---
+++ Algorithms-1/JAVA/demo/src/Quickfire.java
@@ -0,0 +1,61 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Quickfire {
+    private int[] id;
+
+    //initialize
+    public Quickfire(int n) {
+        id = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    //connected or not
+    public boolean connected(int p, int q) {
+        return (id[p] == id[q]);
+    }
+
+    public int find(int p) {
+        validate(p);
+        return id[p];
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n - 1));
+        }
+    }
+
+    //join the nodes
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (pid == id[i]) {
+                id[i] = qid;
+            }
+        }
+    }
+
+    public static void main(String[] arg) {
+        int n = StdIn.readInt();
+        Quickfire obj1 = new Quickfire(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (obj1.find(p) == obj1.find(q)) continue;
+            obj1.union(p, q);
+            StdOut.print(p + " " + q);
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(obj1.id[i]);
+        }
+    }
+}
+
